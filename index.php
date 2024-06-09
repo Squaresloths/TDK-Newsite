e040a20b4a10b5180c17315eaaac499199649450
SquareSloth <raduadumitru70@gmail.com>
2024-06-09 18:32:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $name = "Sloth Square";
+    $food = "pizza";
+    $email = "raduadumitru70@gmail.com";
+
+    $age = 21;
+
+    echo "Hello {$name} <br>";
+    echo "You like {$food} <br>";
+    echo "Your email is {$email} <br>";
+    echo "You are {$age} years old!";
+    //http://localhost:3000/index.php - (use this to log into server!!)
+?>
